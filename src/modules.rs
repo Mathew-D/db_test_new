63d3444a7b873942037ce310d771527af4f6fa12
Mathew-D <mathew_dusome@kprdsb.ca>
2026-04-23 21:18:55 -0400

    Change to new format.

--- src/modules.rs
+++ src/modules.rs
@@ -1,6 +1,6 @@
 /*
 --------------------------------------------
-modules/mod.rs
+modules.rs
 This file lists which modules (Rust files) are part of the "modules" folder.
 
 This file just tells Rust what's available. It's like a directory of all the tools.
